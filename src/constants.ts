69ff992471e80862e5730fc0ca989f603a141695
Vishwaswarup Rath <vishwaswarup.756@gmail.com>
2026-03-27 17:44:48 +0530

    added speaker details first draft

--- src/constants.ts
+++ src/constants.ts
@@ -3,16 +3,16 @@ import { Event } from './types';
 export const EVENTS: Event[] = [
   {
     id: '1',
-    title: 'Cloud Computing Workshop',
-    date: 'April 15, 2026',
+    title: 'Localhost to Cloud Host: Build & Deploy',
+    date: 'April 1, 2026',
     time: '10:00 AM - 1:00 PM',
-    venue: 'Main Auditorium / Zoom',
+    venue: 'LHC 103 AB4',
     shortDescription: 'Learn the fundamentals of AWS and how to deploy your first application.',
     fullDescription: 'This workshop is designed for beginners who want to dive into the world of Cloud Computing. We will cover EC2, S3, and Lambda basics, followed by a hands-on session where you will deploy a static website on AWS.',
     speaker: {
-      name: 'John Doe',
-      designation: 'AWS Certified Solutions Architect',
-      image: 'https://picsum.photos/seed/john/300/300',
+      name: 'Shubham Londhe',
+      designation: 'Senior Developer Advocate',
+      image: 'src/images/shubhamlondhe.jpeg',
     },
   },
   {
