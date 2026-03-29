caf360f128ed91f90f46ef9813e9244861edf95f
Anmol Srivastava <anmolsrivastava073@gmail.com>
2026-03-29 19:18:32 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -9,17 +9,27 @@ import Footer from './components/Footer';
 import LandingPage from './pages/LandingPage';
 import EventDetailsPage from './pages/EventDetailsPage';
 
+import MeetupPage from './pages/MeetupPage';
+import ClassesPage from './pages/ClassesPage';
+import AttendancePage from './pages/AttendancePage';
+
 export default function App() {
   return (
     <Router>
       <div className="min-h-screen flex flex-col bg-aws-black text-white selection:bg-aws-orange selection:text-black">
         <Navbar />
+
         <main className="flex-grow">
           <Routes>
             <Route path="/" element={<LandingPage />} />
             <Route path="/event" element={<EventDetailsPage />} />
+
+            <Route path="/meetup" element={<MeetupPage />} />
+            <Route path="/classes" element={<ClassesPage />} />
+            <Route path="/attendance" element={<AttendancePage />} />
           </Routes>
         </main>
+
         <Footer />
       </div>
     </Router>
