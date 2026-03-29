d86be1b3cd65efa79ac397d7ca20d7e5a2423ac9
Anmol Srivastava <anmolsrivastava073@gmail.com>
2026-03-29 21:46:01 +0530

    Refactor App component layout and styles

--- src/App.tsx
+++ src/App.tsx
@@ -12,19 +12,32 @@ import EventDetailsPage from './pages/EventDetailsPage';
 export default function App() {
   return (
     <Router>
-      <div className="min-h-screen flex flex-col bg-aws-black text-white selection:bg-aws-orange selection:text-black">
+      <div className="relative min-h-screen flex flex-col text-white overflow-hidden">
+
+        {/* 🔥 BACKGROUND GRADIENT */}
+        <div className="fixed inset-0 -z-10 bg-gradient-to-br from-[#050816] via-[#0a0f2c] to-[#020617]" />
+
+        {/* 🔥 GLOW EFFECTS */}
+        <div className="fixed inset-0 -z-10">
+          <div className="absolute top-1/4 left-1/4 w-[400px] h-[400px] bg-indigo-500/20 blur-[140px] rounded-full" />
+          <div className="absolute bottom-1/4 right-1/4 w-[400px] h-[400px] bg-purple-500/20 blur-[140px] rounded-full" />
+        </div>
+
+        {/* Navbar */}
         <Navbar />
 
+        {/* Main */}
         <main className="flex-grow">
           <Routes>
             {/* Home */}
             <Route path="/" element={<LandingPage />} />
 
-            {/* ✅ FIXED: Dynamic Event Route */}
+            {/* Event */}
             <Route path="/event/:id" element={<EventDetailsPage />} />
           </Routes>
         </main>
 
+        {/* Footer */}
         <Footer />
       </div>
     </Router>
